5a3561770e578a92b8113518f72c491e99b576cf
teonakuzmanovska <teona.kuzmanovska@students.finki.ukim.mk>
2025-10-26 20:26:14 +0100

    SCRUM-3 installed dapper and initialized base repo methods

--- 02 Domain/02 Repositories/01 Interfaces/App.Domain.Repositories.Interfaces/Base/IBaseRepository.cs
+++ 02 Domain/02 Repositories/01 Interfaces/App.Domain.Repositories.Interfaces/Base/IBaseRepository.cs
@@ -4,5 +4,42 @@ namespace App.Domain.Repositories.Interfaces.Base;
 
 public interface IBaseRepository<TEntity> where TEntity : class, IBaseDomainEntity<int>, new()
 {
+    /// <summary>
+    /// Inserts a new entity in the db.
+    /// </summary>
+    /// <param name="entity"></param>
+    /// <returns></returns>
+    int Insert(TEntity entity);
+    
+    /// <summary>
+    /// Deletes the entity with the provided id from the db.
+    /// </summary>
+    /// <param name="id"></param>
+    void DeleteById(int id);
+    
+    /// <summary>
+    /// Deletes multiple entities by provided ids.
+    /// </summary>
+    /// <param name="ids"></param>
+    void DeleteByIds(IEnumerable<int> ids);
 
+    /// <summary>
+    /// Returns the entity that matches the given id.
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
+    TEntity GetById(int id);
+    
+    /// <summary>
+    /// Returns all entities that match the provided ids.
+    /// </summary>
+    /// <param name="ids"></param>
+    /// <returns></returns>
+    List<TEntity> GetByIds(IEnumerable<int> ids);
+    
+    /// <summary>
+    /// Returns all entities from the table
+    /// </summary>
+    /// <returns></returns>
+    List<TEntity> GetAll();
 }
--- 02 Domain/02 Repositories/02 Implementations/App.Domain.Repositories.Implementations/BaseRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using App.Domain.Entities.Base;
-using App.Domain.Repositories.Interfaces.Base;
-
-namespace App.Domain.Repositories.Implementations;
-
-public class BaseRepository<T> : IBaseRepository<T> where T : class, IBaseDomainEntity<int>, new()
-{
-
-}
