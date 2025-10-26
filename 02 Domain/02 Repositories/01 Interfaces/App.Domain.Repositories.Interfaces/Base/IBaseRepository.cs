5a5842cc49e582f03095a3580e9851b5d7c9e967
teonakuzmanovska <teona.kuzmanovska@students.finki.ukim.mk>
2025-10-26 20:09:52 +0100

    SCRUM-3 created repository projects

---
+++ 02 Domain/02 Repositories/01 Interfaces/App.Domain.Repositories.Interfaces/Base/IBaseRepository.cs
@@ -0,0 +1,8 @@
+﻿using App.Domain.Entities.Base;
+
+namespace App.Domain.Repositories.Interfaces.Base;
+
+public interface IBaseRepository<TEntity> where TEntity : class, IBaseDomainEntity<int>, new()
+{
+
+}
---
+++ 02 Domain/02 Repositories/02 Implementations/App.Domain.Repositories.Implementations/BaseRepository.cs
@@ -0,0 +1,9 @@
+﻿using App.Domain.Entities.Base;
+using App.Domain.Repositories.Interfaces.Base;
+
+namespace App.Domain.Repositories.Implementations;
+
+public class BaseRepository<T> : IBaseRepository<T> where T : class, IBaseDomainEntity<int>, new()
+{
+
+}
